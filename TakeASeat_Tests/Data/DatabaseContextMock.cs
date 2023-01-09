70795303b8739ca2e9e0d606bbfc3f1e9bf50359
T.T <tomasiktomasz00@gmail.com>
2023-01-09 17:40:39 +0100

    EventsTestsAdded

---
+++ TakeASeat_Tests/Data/DatabaseContextMock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using TakeASeat.Data;
+using TakeASeat.Data.DatabaseContext;
+using Xunit;
+
+namespace TakeASeat_Tests.Data
+{
+    public class DatabaseContextMock
+    {
+        public async Task<DatabaseContext> GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "MockDB")
+                .Options;
+
+            var contextMock = new DatabaseContext(options);
+            contextMock.Database.EnsureCreated();
+            
+            return contextMock;
+        }
+
+        public async void CleanDB()
+        {
+            var context = await GetDatabaseContext();
+            context.Database.EnsureDeletedAsync();
+        }
+    }
+}
--- TakeASeat_Tests/Service/EventRepositoryTest.cs
+++ TakeASeat_Tests/Service/EventRepositoryTest.cs
@@ -5,17 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using FakeItEasy;
+using FluentAssertions;
 using TakeASeat.Data.DatabaseContext;
 using TakeASeat.ProgramConfigurations.DTO;
 using TakeASeat.RequestUtils;
 using TakeASeat.Services.EventService;
 using TakeASeat.Services.EventTagRepository;
+using TakeASeat_Tests.Data;
 
 namespace TakeASeat_Tests.Service
 {
     public class EventRepositoryTest
     {
                 
+        private readonly IEventTagRepository _eventTagRepository;
+        private readonly IMapper _mapper;
+        private readonly DatabaseContextMock _DbMock;
+
+        public EventRepositoryTest()
+        {
+            _eventTagRepository = A.Fake<IEventTagRepository>();
+            _mapper = A.Fake<IMapper>();
+            _DbMock = new DatabaseContextMock();
+        }
+
+        [Fact]
+        public async void EventRepository_GetEventRecordsNumber_ReturnInt()
+        {
+            // arrange
+            var context = await _DbMock.GetDatabaseContext();
+            EventRepository repository = new EventRepository(context, _mapper, _eventTagRepository);
+
+            // act
+            var response = await repository.GetEventRecordsNumber();
+
+            // assert
+            response.Should().BeOfType(typeof(int));
+        }
+
+        [Fact]
+        public async void EventRepository_GetEventRecordsNumber_ReturnNumber5()
+        {
+            // arrange
+            var context = await _DbMock.GetDatabaseContext();
+            EventRepository repository = new EventRepository(context, _mapper, _eventTagRepository);
+
+            // act
+            var response = await repository.GetEventRecordsNumber();
+
+            // assert
+            response.Should().Be(5);
+        }
+
+
         [Fact]
         public void EventRepository_CreateEvent_Slugify()
         {
